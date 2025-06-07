04c3dc0024278c90480415f9fe08fc6b71e18392
kiralightyagami <crucisgamer98@gmail.com>
2025-06-07 17:29:21 +0530

    connecting to backend

--- apps/frontend/components/prompt.tsx
+++ apps/frontend/components/prompt.tsx
@@ -2,13 +2,29 @@
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 import { Send} from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { BACKEND_URL } from "@/config";
 
 export function Prompt() {
+  const [prompt, setPrompt] = useState("");
+  const { getToken } = useAuth();
     return (
         <div className="flex flex-col gap-2">
-          <Textarea placeholder="Enter your prompt here" className="w-full h-20"/>
+          <Textarea placeholder="Enter your prompt here" value={prompt} onChange={(e) => setPrompt(e.target.value)} className="w-full h-20"/>
           <div className="flex justify-end pt-2">
-            <Button>
+            <Button onClick={async () => {
+              const token = await getToken();
+              const response = await axios.post(`${BACKEND_URL}/project`, {
+                prompt: prompt
+              },{
+                headers: {
+                  "Authorization": `Bearer ${token}`
+                } 
+              })
+              console.log(response)
+            }}>
                 <Send />
             </Button>
           </div>
